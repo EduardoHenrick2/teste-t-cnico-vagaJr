abb539b1270c272de14c71e9c66af791f1d68fd4
Eduardo Lopes <eduardohenrick900@gmail.com>
2026-05-11 17:51:10 -0300

    teste de Fibonacci

---
+++ teste-Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Fibonacci(int n)
+    {
+        if (n == 0) 
+            return;
+
+        long a = 0, b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a);
+
+            if (i < n - 1) 
+                Console.Write(", ");
+
+            long temp = a + b;
+            a = b;
+            b = temp;
+        }
+        Console.WriteLine();
+    }
+
+    public static void Main(string[] args)
+    {
+        Fibonacci(3);
+        Fibonacci(5);
+        Fibonacci(7);
+    }
+}
